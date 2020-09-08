033a4cb11b524acae71b823d472c6d5347942543
Randy <randanator1227@gmail.com>
2020-09-07 18:59:06 -0600

    Finished Assignment 0 Sorry im late

---
+++ Cyborg Shrimp/Assets/MoverScript.cs
@@ -0,0 +1,37 @@
+﻿
+using UnityEngine;
+
+public class MoverScript : MonoBehaviour
+{
+    public float speed = 5f;
+    public float health = 100f;
+    public int points = 60;
+    public string password = "12345";
+    public string playerName = "Randanator";
+    private void Update()
+    {
+        var vInput = speed * Time.deltaTime * Input.GetAxis("Vertical");
+        var hInput = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        transform.Translate(hInput, vInput, 0);
+    }
+
+    public void Up()
+    {
+        transform.Translate(0, speed, 0);
+    }
+
+    public void Down()
+    {
+        transform.Translate(0, -speed, 0);
+    }
+
+    public void Left()
+    {
+        transform.Translate(-speed, 0, 0);
+    }
+
+    public void Right()
+    {
+        transform.Translate(speed, 0, 0);
+    }
+}
