8d41fd24dad78db4150b62ae5f149e57720c5132
Randy <randanator1227@gmail.com>
2020-09-10 16:04:31 -0600

    finished the homework for our game

---
+++ Cyborg Shrimp/Assets/AmmoUp.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoUp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int ammo;
+    private void OnTriggerEnter(Collider other)
+    {
+        ammo++;
+        print(ammo);
+    }
+}
---
+++ Cyborg Shrimp/Assets/ArmorUp.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmorUp : MonoBehaviour
+{
+    public int armor;
+    private void OnTriggerEnter(Collider other)
+    {
+        armor++;
+        print(armor);
+    }
+}
