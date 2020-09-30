b62e8f51212cfe7300211686487d5123b26c2868
Randy <randanator1227@gmail.com>
2020-09-30 11:31:38 -0600

    finished first challenge

--- Create With Code/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Create With Code/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -5,14 +5,8 @@ public class FollowPlayer : MonoBehaviour
     public GameObject player;
     private Vector3 camera;
     public Vector3 offset = new Vector3(0, 5, -10);
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         camera = player.transform.position + offset;
         transform.position = camera;
