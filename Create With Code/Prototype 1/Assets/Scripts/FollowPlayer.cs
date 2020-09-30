26b0141541b131f47041e215fb1803899d2336eb
Randy <randanator1227@gmail.com>
2020-09-30 10:28:27 -0600

    finished the first tutorial of car driving into boxes

---
+++ Create With Code/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 camera;
+    public Vector3 offset = new Vector3(0, 5, -10);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camera = player.transform.position + offset;
+        transform.position = camera;
+    }
+}
