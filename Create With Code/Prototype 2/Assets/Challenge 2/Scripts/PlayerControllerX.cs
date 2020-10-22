7d9c5e66d9addf1955ed13222fca1e451cc76e29
Randy <randanator1227@gmail.com>
2020-10-21 19:42:00 -0600

    creat with code prototype 2 edits

---
+++ Create With Code/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+
+namespace Challenge_2.Scripts
+{
+    public class PlayerControllerX : MonoBehaviour
+    {
+        public GameObject dogPrefab;
+
+        // Update is called once per frame
+        void Update()
+        {
+            // On spacebar press, send dog
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartCoroutine("SendDog");
+            }
+        }
+
+        void SendDog()
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+}
