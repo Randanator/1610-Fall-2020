d4ba9bdf52819128f784e5e518a1e862ec9a0cae
Randy <randanator1227@gmail.com>
2020-10-25 03:17:17 -0600

    work on personal project in lab 3

---
+++ Create With Code/Personal Project/Randy Lazenby - Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    public Rigidbody playerRb;
+    public float speed = 1000f;
+    public float verticalInput;
+    public float horizontalInput;
+    
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(Vector3.forward * (speed * verticalInput * Time.deltaTime));
+        playerRb.AddForce(Vector3.right * (speed * horizontalInput * Time.deltaTime));
+    }
+}
