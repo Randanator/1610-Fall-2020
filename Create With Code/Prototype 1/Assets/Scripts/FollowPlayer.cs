ca365a191f96bb2631bd7959aefa1ff83a68f22e
Randy <randanator1227@gmail.com>
2020-11-20 19:50:48 -0700

    updated code trying new code methods on prototype 1

--- Create With Code/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Create With Code/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -3,12 +3,10 @@
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 camera;
-    public Vector3 offset = new Vector3(0, 5, -10);
+    [SerializeField] Vector3 offset = new Vector3(0, 0, 0);
     // Update is called once per frame
-    public void Update()
+    public void LateUpdate()
     {
-        camera = player.transform.position + offset;
-        transform.position = camera;
+        transform.position = player.transform.position + offset;
     }
 }
--- Create With Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,32 +1,24 @@
 ﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{ 
-    // Used for forward math calculations
-    private Vector3 forward;
-    // Used for rotational math calculations
-    private Vector3 rotate;
+{
     // variable for speed
-    private float speed = 20f;
+    [SerializeField] float speed = 20f;
     // variable for turning rotation
-    private float turnSpeed = 45f;
+    [SerializeField] float turnSpeed = 45f;
     // variable for the horizontal input gathering
     private float horizontalInput;
     // variable for vertical input gathering
     private float verticalInput;
-   // Update is called once per frame
-    void Update()
+    // Update is called once per frame
+    void FixedUpdate()
     {
-        // does the math for the transform.Translate(forward)
-        forward = Vector3.forward * (Time.deltaTime * speed * verticalInput);
-        // does the math for the transform.Translate(rotate)
-        rotate = Vector3.up * (Time.deltaTime * turnSpeed * horizontalInput);
         // these get the inputs
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
         // moves the car forward based on forward math
-        transform.Translate(forward);
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed * verticalInput));
         // rotates the car based on rotate math
-        transform.Rotate(rotate);
+        transform.Rotate(Vector3.up * (Time.deltaTime * turnSpeed * horizontalInput));
     }
 }
